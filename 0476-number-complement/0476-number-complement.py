161136ddb5d16a1aede3e52038202849697013ba
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-03-30 17:47:05 +0300

    Time: 28 ms (81.28%), Space: 13.9 MB (40.50%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        i = 1
+        while(i <=num):
+            i= i << 1
+        candidate = i - 1
+        
+        return (num ^ candidate)
+         
+        
+       
+        
+        

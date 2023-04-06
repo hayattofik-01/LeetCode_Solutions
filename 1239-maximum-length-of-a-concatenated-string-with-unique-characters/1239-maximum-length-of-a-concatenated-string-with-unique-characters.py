766549a24e62c8a099aae5a1f8cd962db15ab8d7
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-04-06 11:46:50 +0300

    Time: 106 ms (63.07%), Space: 14 MB (32.59%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,44 +1,94 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        max_length = float('-inf')
+        visited = set()
         
-        # use prefix_size to check if the remaining array can produce possible larger unique string
-        prefix_size = [0] * len(arr)
-        prefix_size[-1] = len(set(list(arr[-1])))
-        
-        for i in range(len(arr)-2, -1, -1):
-            prefix_size[i] = len(set(list(arr[i]))) + prefix_size[i+1]    
-        
-        # print(prefix_size)
-        def backtrack(arr, string, index=0):
-            nonlocal max_length
-            # before we go deeper backtrack, 
-            # we check if the remaining array can produce possible larger unique string,
-            # if current string, presumbly unique, plus the remaining possible unique string has length smaller than current maximum length, 
-			# then we don't need to go deeper, just return.
-            if index < len(prefix_size) and len(string) + prefix_size[index] < max_length:
-                return
+        def notRepeated(array):
             
-            if len(string) > max_length and len(string) > 0:
-                max_length = len(string)
+            prev =set()
             
-            if index == len(arr):
-                return
-            
-            for i in range(index, len(arr)):
-                set1 = set(string)
-                set2 = set(list(arr[i]))
-                union = set1.union(set2)
+            for i in range(len(array)):
                 
-                # must check if the union set size is equal to the array size sum so to make sure they are all unique
-                if len(union) == len(string) + len(arr[i]):
-                    for c in set2:
-                        string.append(c)
-                    backtrack(arr, string, i + 1)
-                    for c in range(len(arr[i])):
-                        string.pop()
-            return
+                if array[i] in prev or array[i] in visited:
+                    return False
+                prev.add(array[i])
+            return True
         
+        def backTrack(i):
+            
+            if i == len(arr):
+                return len(visited)
+            
+            res = 0 
+            
+            if notRepeated(arr[i]):
+                for l in arr[i]:
+                    visited.add(l)
+                    
+                res = backTrack(i+1)
+                for l in arr[i]:
+                    visited.remove(l)
+            return max(res,backTrack(i+1))
+        return backTrack(0)
+            
+                    
+                
+       
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+#             cnt += 1
+            
+#             if i > len(lis):
+#                 return 
+            
+#             for i in range(cur,len(arr)):
+                
+#                 for  i 
         
-        backtrack(arr, [], 0)
-        return max_length if max_length != float('-inf') else 0
+#         for i in range(len(arr)):
+#             visited = set()
+#             dupl = False
+#             for j in arr[i]:
+#                 if j in visited:
+#                     dupl = True
+#                     break
+#                 visited.add(j)
+            
+#             if not dupl:
+#                 backTrack(i+1, cnt)
+            
+            
+                
+                
+#         return 9
+            
+            
+            
+            

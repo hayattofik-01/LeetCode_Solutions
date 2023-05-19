7cb324b36ae36314497fe2613ea174eccd461ea6
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-05-19 18:51:20 +0300

    Time: 4078 ms (5.45%), Space: 124.2 MB (44.24%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -9,28 +9,37 @@ class Solution:
         dxnRef[6].update({(-1,0),(0,1)})
     
         parent = defaultdict(tuple)
+        size = {}
         
         for row in range(len(grid)):
              for col in range(len(grid[0])):
                 parent[(row,col)] = (row,col)
+                size[(row,col)] = 1
+                
         def inBound(row,col):
             return 0 <= row < len(grid) and 0 <= col < len(grid[0])
         
-        def findRoot(row, col):
+        def find(cell):
+            
+            if parent[cell] != cell:
+                parent[cell] = find(parent[cell])
+            
+            return parent[cell]
            
-            while parent[(row,col)] != (row,col):
-                
-                row, col =  parent[(row,col)][0],parent[(row,col)][1]
-            return (row,col)
         def union(cell1,cell2):
             
-            rootC1 = findRoot(cell1[0],cell1[1])
-            rootC2 = findRoot(cell2[0],cell2[1])
+            rootC1 = find(cell1)
+            rootC2 = find(cell2)
             
             if rootC1 != rootC2:
                 
-                parent[rootC1] = rootC2
-                
+                if size[rootC1] >= size[rootC2]:
+                    parent[rootC2] = rootC1
+                    size[rootC1] += size[rootC2]
+                else:
+                    parent[rootC1] = rootC2
+                    size[rootC2] += size[rootC1]
+                    
                 
         for row in range(len(grid)):
             for col in range(len(grid[0])):
@@ -41,19 +50,11 @@ class Solution:
                     new_r ,new_c = row + dxn[0],col + dxn[1]
                    
                     if inBound(new_r,new_c) and (-1* dxn[0],-1* dxn[1]) in dxnRef[grid[new_r][new_c]]:
-                       
-                            
                             union((row,col),(new_r,new_c))
+                            
         m  = len(grid) -1
         n = len(grid[0]) -1
-        if  findRoot(0,0) == findRoot(m,n):
-            return True
-        return False
-                
-                
         
-        
-            
-    
+        return find((0,0)) == find((m,n))
         
         

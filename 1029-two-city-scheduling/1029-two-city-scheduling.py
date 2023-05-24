aaa02761cbc03ffae1488d02251b7c9ba788f748
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-05-24 19:33:52 +0300

    Time: 67 ms (9.30%), Space: 16.3 MB (49.49%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        costs.sort(key= lambda a : a[0] - a[1])
+        ans = 0
+        for i,cost in enumerate(costs):
+            if i < len(costs)//2:
+                ans +=cost[0]
+            else:
+                ans+=cost[1]
+        return ans
+        
+        
+        
+        
+        
+        
+       
+            
+        

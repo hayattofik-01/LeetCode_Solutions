03d014a3e690e456b3751bf20623693457c67bd8
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-05-26 19:54:27 +0300

    Time: 46 ms (28.64%), Space: 16.2 MB (31.76%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def tribonacci(self, n: int,memo = defaultdict(int)) -> int:
+        
+        if n == 0:
+            return 0
+        if n <=  2:
+            return 1
+        if n in memo:
+            return memo[n]
+        
+        memo[n] =  self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        return  memo[n]

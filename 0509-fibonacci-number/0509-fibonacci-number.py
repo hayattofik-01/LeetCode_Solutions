9d88d9ab800bf8362c9af33309f5d8b7b44771bd
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-05-26 17:44:52 +0300

    Time: 49 ms (42.67%), Space: 16.3 MB (24.39%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def __init__(self):
+        self.memo = defaultdict(int)
+    def fib(self, n: int) -> int:
+    
+        if n  <= 1:
+            return n
+        
+        if n in self.memo:
+            return self.memo[n]
+        
+        self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        
+        return self.memo[n]
+        
+        
+        

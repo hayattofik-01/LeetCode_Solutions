8ea6172ee29a1529e677883c191c1590297e566f
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-05-31 19:33:59 +0300

    Time: 42 ms (49.35%), Space: 16.4 MB (9.90%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,24 +1,23 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        path = 0
-        memo = {}
+        
+        memo= [[0 for i in range(n)] for i in range(m) ]
+        print(memo)
         def inBound(i,j):
-            return 0 <=  i < m and 0 <= j < n
+            if  0 <=  i < m and 0 <= j < n:
+                return memo[i][j]
+            return 0
         
-        def dfs(i,j):
-            nonlocal path
-            if not inBound(i,j):
-                return 0 
-            if (i,j) == (m-1,n-1):
-               
-                return  1
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            memo[(i,j)] = dfs(i + 1, j) + dfs(i,j + 1)
-            
-            
-            return memo[(i,j)]
+       
+        memo[0][0] = 1
         
-        return dfs(0,0)
-        
+        for r in range(m):
+            
+            for c in range(n):
+                
+                memo[r][c] += inBound(r - 1,c) + inBound(r,c-1)
+                
+        return memo[m-1][n-1]
+                
+                
+                

9331eecb48cf82feb5e40ba52d4c440c10563509
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-09-07 01:42:57 -0700

    Time: 575 ms (70.31%), Space: 19.6 MB (65.91%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        def getBitmask(word):
+            bitmask = 0
+            for char in word:
+                bitmask |= 1 << (ord(char) - ord('a'))
+            return bitmask
+
+        word_bitmasks = {}
+        for word in words:
+            word_bitmasks[word] = getBitmask(word)
+
+        max_product = 0
+        n = len(words)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if word_bitmasks[words[i]] & word_bitmasks[words[j]] == 0:
+                    max_product = max(max_product, len(words[i]) * len(words[j]))
+
+        return max_product

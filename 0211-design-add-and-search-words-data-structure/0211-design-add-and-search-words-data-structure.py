66224bd31396d82888481d16ece97f8977f29a8d
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-09-26 15:59:39 +0300

    Time: 3819 ms (8.32%), Space: 93.2 MB (5.61%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -17,20 +17,19 @@ class WordDictionary:
     def searchRecursive(self, word: str, node) -> bool:
         if not word:
             return node.is_end
-
-        if word[0] == '.':
+        
+        if word[0] == ".":
             for child in node.children:
-                if child and self.searchRecursive(word[1:], child):
+                if child and self.searchRecursive(word[1:],child):
                     return True
             return False
-
         if not node.children[ord(word[0]) - ord('a')]:
             return False
-
-        return self.searchRecursive(word[1:], node.children[ord(word[0]) - ord('a')])
+        return self.searchRecursive(word[1:],node.children[ord(word[0]) - ord('a')])
+       
 
 
 class TrieNode:
     def __init__(self):
-        self.children = [None] * 26
+        self.children = [None for i in range(26)]
         self.is_end = False

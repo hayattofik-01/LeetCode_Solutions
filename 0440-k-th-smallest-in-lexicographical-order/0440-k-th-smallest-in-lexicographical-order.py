74d85f7a91f9a9258799c8ae7f1b15cc1393bed3
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-10-08 08:44:45 +0300

    Time: 43 ms (25.00%), Space: 16.2 MB (83.72%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -1,23 +1,26 @@
 class Solution:
     def findKthNumber(self,n, k):
         current = 1
-        k -= 1
+        k -= 1  # Decrement k to account for the first number we start with
 
         while k > 0:
             count = 0
-            interval = [current, current + 1]
 
-            while interval[0] <= n:
-                count += min(n + 1, interval[1]) - interval[0]
-                interval = [10 * interval[0], 10 * interval[1]]
+            # Calculate the count of numbers between current and current + 1
+            interval_start = current
+            interval_end = current + 1
+
+            while interval_start <= n:
+                count += min(n + 1, interval_end) - interval_start
+                interval_start *= 10
+                interval_end *= 10
 
             if k >= count:
-                current += 1
+                current += 1  # Move to the next number in the same level
                 k -= count
             else:
-                current *= 10
+                current *= 10  # Move to the next level by appending a 0
                 k -= 1
 
         return current
-
 

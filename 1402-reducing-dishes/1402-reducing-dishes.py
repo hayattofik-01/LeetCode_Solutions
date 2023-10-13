0a23f28bec5ac0e605b94f4581ec84bbf24d1689
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-10-13 14:48:35 +0300

    Time: 115 ms (34.19%), Space: 16.4 MB (57.47%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -2,12 +2,6 @@ class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()  # Sort in non-decreasing order
         n = len(satisfaction)
-        prefix_sum = [0] * (n + 1)  # Prefix sum array
-
-        # Calculate prefix sum
-        for i in range(n - 1, -1, -1):
-            prefix_sum[i] = satisfaction[i] + prefix_sum[i + 1]
-
         max_cof = 0
 
         # Calculate maximum like-time coefficient

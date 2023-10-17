61a5ad11fa66988e6383433773b72c976a60880d
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-10-17 14:29:11 +0300

    Time: 158 ms (28.44%), Space: 20.1 MB (19.73%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        # build nums_list
+        l = r = 0
+        nums = deque()
+        while r < len(s):
+            if s[r] in ['+', '-', '*', '/']:
+                nums.append(int(s[l:r]))
+                nums.append(s[r])
+                l = r+1
+            r += 1
+        nums.append(int(s[l:r]))
+
+        add = []
+        # calculate * and /
+        while nums:
+            current = nums.popleft()
+            if current == '*':
+                add[-1] = int(add[-1] * nums.popleft())
+            elif current == '/':
+                add[-1] = int(add[-1] / nums.popleft())
+            elif current == '+':
+                add.append(nums.popleft())
+            elif current == '-':
+                add.append(-nums.popleft())
+            
+            else:
+                add.append(current)
+        
+        return sum(add)
+             

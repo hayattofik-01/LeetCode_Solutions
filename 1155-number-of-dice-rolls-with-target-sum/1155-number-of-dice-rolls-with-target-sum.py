14d667856e533f6b9a3ad08b2ab04772577b527e
Hayat Tofik <129114795+hayattofik-01@users.noreply.github.com>
2023-11-08 15:41:09 +0300

    Time: 648 ms (16.86%), Space: 16.6 MB (84.29%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+         mod = 10**9 + 7
+         dp = [[0] * (target+1) for _ in range(n+1)]
+         dp[0][0] = 1
+
+         for i in range(1, n+1):
+             for j in range(1, target+1):
+                 for x in range(1, min(k, j)+1):
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-x]) % mod
+
+         return dp[n][target]
+
+
+
+                
+                
+        
